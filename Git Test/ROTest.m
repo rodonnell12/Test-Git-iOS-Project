f243a5bcb1a21b51b67ca33079ce145a079be568
Reilly ODonnell <reillyodonnell@Reillys-iMac.local>
2014-02-24 22:48:29 -0500

    added a test class

---
+++ Git Test/ROTest.m
@@ -0,0 +1,13 @@
+//
+//  ROTest.m
+//  Git Test
+//
+//  Created by Reilly ODonnell on 2/24/14.
+//  Copyright (c) 2014 Reilly ODonnell. All rights reserved.
+//
+
+#import "ROTest.h"
+
+@implementation ROTest
+
+@end
